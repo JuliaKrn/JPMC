29b30e77cdc0b587ada75a9c04f2dce3a1d4ddf6
Yulia Korniichuk <yuliakorniichuk@appyway.com>
2023-05-20 11:48:47 +0100

    Initial Commit

---
+++ Yulia_Korniichuk_GPMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yulia_Korniichuk_GPMC
+//
+//  Created by Yulia Kornichuk on 20/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
